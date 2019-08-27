8189fd9bd6b4cb2174e90d6c2ac0cfa5aeb3ddee
Pavlo Kerestey <pavlo@kerestey.net>
2019-08-27 22:16:16 +0200

    Try running the deployment script on the agent directly

--- infra/scripts/deploy.sh
+++ infra/scripts/deploy.sh
@@ -9,8 +9,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # shellcheck source=infra/scripts/lib.shs
 source "${DIR}/variables.sh"
 
-docker save ${IMAGE_SMTPD} | bzip2 -9 | pv | ssh -oStrictHostKeyChecking=no ${REMOTE} 'mkdir -p /opt/unverified.email/; bunzip2 | docker load'
-docker save ${IMAGE_API} | bzip2 -9 | pv | ssh -oStrictHostKeyChecking=no ${REMOTE} 'mkdir -p /opt/unverified.email/; bunzip2 | docker load'
+docker save ${IMAGE_SMTPD} | bzip2 -9 | ssh -oStrictHostKeyChecking=no ${REMOTE} 'mkdir -p /opt/unverified.email/; bunzip2 | docker load'
+docker save ${IMAGE_API} | bzip2 -9 | ssh -oStrictHostKeyChecking=no ${REMOTE} 'mkdir -p /opt/unverified.email/; bunzip2 | docker load'
 scp -oStrictHostKeyChecking=no "infra/nomad-definitions.hcl" "${REMOTE}:/opt/unverified.email/nomad-definitions.hcl"
 ssh -oStrictHostKeyChecking=no "${REMOTE}" 'nomad job run /opt/unverified.email/nomad-definitions.hcl'
 
