1af251dd8d4020931266a2c77c5a420b439b702f
Pavlo Kerestey <pavlo@kerestey.net>
2019-08-27 21:58:12 +0200

    Add deployment step to the pipeline. The agent is now configured to connect to do

--- infra/scripts/variables.sh
+++ infra/scripts/variables.sh
@@ -1,4 +1,3 @@
 export REMOTE="root@api.unverified.email"
 export IMAGE_SMTPD="unverified.email/smtpd:unversioned"
 export IMAGE_API="unverified.email/api:unversioned"
-export SSH_PRIVATE_KEY="./secrets/unverified.email-deployment-ed25519"
