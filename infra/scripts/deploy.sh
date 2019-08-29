541eeac51fe0016badc979ff6157399e75899e2b
Pavlo Kerestey <pavlo@kerestey.net>
2019-08-29 02:52:02 +0200

    Deploy via docker compose with traefik v2.0 as ssl termination

--- infra/scripts/deploy.sh
+++ infra/scripts/deploy.sh
@@ -3,6 +3,7 @@
 set -e
 set -u
 set -o pipefail
+set -x
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -12,9 +13,9 @@ source "${DIR}/../variables.sh"
 docker save "${IMAGE_SMTPD}" | bzip2 -9 | ssh -oStrictHostKeyChecking=no "${REMOTE}" 'mkdir -p /opt/unverified.email/; bunzip2 | docker load'
 docker save "${IMAGE_API}" | bzip2 -9 | ssh -oStrictHostKeyChecking=no "${REMOTE}" 'mkdir -p /opt/unverified.email/; bunzip2 | docker load'
 
-envsubst < infra/nomad-definitions.hcl | ssh -oStrictHostKeyChecking=no "${REMOTE}" "cat > /opt/unverified.email/nomad-definitions.hcl"
-ssh -oStrictHostKeyChecking=no "${REMOTE}" 'nomad job run /opt/unverified.email/nomad-definitions.hcl'
+envsubst < infra/docker-compose.yaml | ssh -oStrictHostKeyChecking=no "${REMOTE}" "cat > /opt/unverified.email/docker-compose.yaml"
+ssh -oStrictHostKeyChecking=no "${REMOTE}" 'cd /opt/unverified.email/ && docker-compose up -d'
 
 sleep 5
-ssh -oStrictHostKeyChecking=no "${REMOTE}" '(nomad status unverified.email | grep -E "Status\s+= running" > /dev/null && echo "OK") || (nomad status unverified.email; exit 1)'
+ssh -oStrictHostKeyChecking=no "${REMOTE}" 'docker ps -a'
 
