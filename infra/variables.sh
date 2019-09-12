c26c7d537e7743e3b5b685dd3c98e4c17e1e0492
Pavlo Kerestey <pavlo@kerestey.net>
2019-09-12 20:50:36 +0200

    Use the old host again

--- infra/variables.sh
+++ infra/variables.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#export REMOTE="root@api.unverified.email"
-export REMOTE="root@167.71.12.112"
+export REMOTE="root@api.unverified.email"
 COMMIT_ID="$(git rev-parse HEAD)" && export COMMIT_ID
 export IMAGE_SMTPD="unverified.email/smtpd:${COMMIT_ID}"
 export IMAGE_API="unverified.email/api:${COMMIT_ID}"
